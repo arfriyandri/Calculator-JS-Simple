f5fa58e9566a881094ead090d7a964e1641a6c35
arfriyandri <arfriyandri2042@gmail.com>
2024-09-09 12:33:16 -0700

    update external js

---
+++ script.js
@@ -0,0 +1,22 @@
+function functionAc() {
+  document.getElementById("display").value = "";
+}
+
+function myFunction(event) {
+  if (
+    event == "1" ||
+    event == "2" ||
+    event == "3" ||
+    event == "4" ||
+    event == "5" ||
+    event == "6" ||
+    event == "7" ||
+    event == "8" ||
+    event == "9" ||
+    event == "0" ||
+    event == "00"
+  ) {
+    // document.getElementById('display').value += en
+    console.log(event);
+  }
+}
